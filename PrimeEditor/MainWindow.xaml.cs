d3964ec14705264f7b30944a85668867ccc36d36
Smax Desktop <home.maxi.schmidt@gmail.com>
2023-09-24 20:24:52 +0200

    implemented null checks for textbox == null

--- PrimeEditor/MainWindow.xaml.cs
+++ PrimeEditor/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -22,7 +23,8 @@ namespace PrimeEditor
         public MainWindow()
         {
             InitializeComponent();
-            CreateNewTab();
+            CreateAddNewTabButton();
+            //CreateNewTab();
 
             // Set Main Window Icon
             Icon = new BitmapImage(new Uri("../../../Images/edit_text_icon.png", UriKind.RelativeOrAbsolute));
@@ -52,7 +54,7 @@ namespace PrimeEditor
                 PrimeEditorFile file = new PrimeEditorFile();
                 file.FilePath = openFileDialog.FileName;
                 file.Content = File.ReadAllText(file.FilePath);
-                
+
                 var newTextBox = CreateNewTab();
 
                 newTextBox.Text = file.Content;
@@ -75,17 +77,22 @@ namespace PrimeEditor
             string tabId = GetSelectedTabId();
             TextBox textBox = GetTextEditorTextBox(tabId);
             PrimeEditorFile file = new PrimeEditorFile();
-            file.FilePath = ((TextBoxData)textBox.Tag).FilePath;
-            file.Content = textBox.Text;
 
-            if (File.Exists(file.FilePath))
+            if (textBox != null)
             {
-                File.WriteAllText(file.FilePath, file.Content);
-                StatusMessage.Content = $"Datei '{file.FileName}' wurde gespeichert.";
-            }
-            else
-            {
-                SaveFileAs_Click(sender, e);
+
+                file.FilePath = ((TextBoxData)textBox.Tag).FilePath;
+                file.Content = textBox.Text;
+
+                if (File.Exists(file.FilePath))
+                {
+                    File.WriteAllText(file.FilePath, file.Content);
+                    StatusMessage.Content = $"Datei '{file.FileName}' wurde gespeichert.";
+                }
+                else
+                {
+                    SaveFileAs_Click(sender, e);
+                }
             }
         }
 
@@ -96,22 +103,25 @@ namespace PrimeEditor
         /// <param name="e"></param>
         private void SaveFileAs_Click(object sender, RoutedEventArgs e)
         {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "Text file (*.txt)|*.txt|C# file(*.cs)|*.cs";
-            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            if (saveFileDialog.ShowDialog() == true)
+            string tabId = GetSelectedTabId();
+            TextBox textBox = GetTextEditorTextBox(tabId);
+            if (textBox != null)
             {
-                string tabId = GetSelectedTabId();
-                TextBox textBox = GetTextEditorTextBox(tabId);
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Text file (*.txt)|*.txt|C# file(*.cs)|*.cs";
+                saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                if (saveFileDialog.ShowDialog() == true)
+                {
 
-                PrimeEditorFile file = new PrimeEditorFile();
-                file.FilePath = saveFileDialog.FileName;
-                file.Content = textBox.Text;
+                    PrimeEditorFile file = new PrimeEditorFile();
+                    file.FilePath = saveFileDialog.FileName;
+                    file.Content = textBox.Text;
 
-                File.WriteAllText(file.FilePath, file.Content);
-                SetSelectedTabItemHeader(file.FileName);
+                    File.WriteAllText(file.FilePath, file.Content);
+                    SetSelectedTabItemHeader(file.FileName);
 
-                StatusMessage.Content = $"Datei '{file.FileName}' wurde gespeichert.";
+                    StatusMessage.Content = $"Datei '{file.FileName}' wurde gespeichert.";
+                }
             }
         }
 
@@ -136,9 +146,12 @@ namespace PrimeEditor
         {
             string tabId = GetSelectedTabId();
             TextBox textBox = GetTextEditorTextBox(tabId);
-            return textBox.Text.Length.ToString();
+            if (textBox != null)
+            {
+                return textBox.Text.Length.ToString();
+            }
+            return string.Empty;
         }
-
 
         /// <summary>
         /// Count the number of words
@@ -148,10 +161,15 @@ namespace PrimeEditor
         {
             string tabId = GetSelectedTabId();
             TextBox textBox = GetTextEditorTextBox(tabId);
-            string text = textBox.Text.Trim();
-            string[] words = GetWords(text);
 
-            return words.Length.ToString();
+            if (textBox != null)
+            {
+                string text = textBox.Text.Trim();
+                string[] words = GetWords(text);
+
+                return words.Length.ToString();
+            }
+            return string.Empty;
         }
 
         /// <summary>
@@ -167,6 +185,12 @@ namespace PrimeEditor
             return Regex.Split(text, pattern);
         }
 
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            WordCount.Content = CountWords();
+            CharacterCount.Content = CountCharacters();
+        }
+
         /// <summary>
         /// Close the Editor
         /// </summary>
@@ -174,6 +198,7 @@ namespace PrimeEditor
         /// <param name="e"></param>
         private void CloseEditor_Click(object sender, RoutedEventArgs e)
         {
+            //TODO: Unsaved changes?
             this.Close();
         }
 
@@ -184,23 +209,28 @@ namespace PrimeEditor
         /// <param name="e"></param>
         private void TextWrap_Click(object sender, RoutedEventArgs e)
         {
+            //TODO: buggy
             string tabId = GetSelectedTabId();
             TextBox textBox = GetTextEditorTextBox(tabId);
-            PrimeEditorFile file = new PrimeEditorFile();
-            file.FilePath = textBox.Name;
-            file.Content = textBox.Text;
 
-            if (file.TextWrapping)
+            if (textBox != null)
             {
-                textBox.TextWrapping = TextWrapping.NoWrap;
-                file.TextWrapping = false;
-                TextWrap.Header = "Enable Text Wrapping";
-            }
-            else
-            {
-                textBox.TextWrapping = TextWrapping.Wrap;
-                file.TextWrapping = true;
-                TextWrap.Header = "Disable Text Wrapping";
+                PrimeEditorFile file = new PrimeEditorFile();
+                file.FilePath = textBox.Name;
+                file.Content = textBox.Text;
+
+                if (file.TextWrapping)
+                {
+                    textBox.TextWrapping = TextWrapping.NoWrap;
+                    file.TextWrapping = false;
+                    TextWrap.Header = "Enable Text Wrapping";
+                }
+                else
+                {
+                    textBox.TextWrapping = TextWrapping.Wrap;
+                    file.TextWrapping = true;
+                    TextWrap.Header = "Disable Text Wrapping";
+                }
             }
         }
 
@@ -215,7 +245,7 @@ namespace PrimeEditor
             newTab.Header = tabName;
 
 
-            TextBoxData data = new TextBoxData{ TabId = tabCounter++.ToString()};
+            TextBoxData data = new TextBoxData { TabId = tabCounter++.ToString() };
             TextBox textBox = CreatePrimeEditorTextBox(data.TabId);
             textBox.TextChanged += TextBox_TextChanged;
 
@@ -226,59 +256,68 @@ namespace PrimeEditor
             tabControl.Items.Insert((tabControl.Items.Count - 1), newTab);
 
             tabControl.SelectedItem = newTab;
-            
+
             return textBox;
+        }
+
+        public void CreateAddNewTabButton()
+        {
+            TabItem tabItem = new TabItem
+            {
+                Header = "+",
+                Style = (Style)FindResource("CustomTabItemStyle"),
+                Tag = new TextBoxData { TabId = tabCounter++.ToString() }
+            };
+            tabItem.MouseLeftButtonUp += AddNewTab_Click;
+            tabControl.Items.Add(tabItem);
         }
 
         private void CloseTab_Click(object sender, RoutedEventArgs e)
         {
-            if (tabControl.Items.Count > 2) 
+            string selectedTabId = GetSelectedTabId();
+            TextBox textBox = GetTextEditorTextBox(selectedTabId);
+
+            if (textBox.Text.Length == 0)
             {
-                string selectedTabId = GetSelectedTabId();
-                TextBox textBox = GetTextEditorTextBox(selectedTabId);
+                CloseTab(sender);
+            }
+            else
+            {
+                PrimeEditorFile file = new PrimeEditorFile();
+                file.FilePath = ((TextBoxData)textBox.Tag).FilePath;
+                file.Content = textBox.Text;
 
-                if (textBox.Text.Length == 0)
+                string messageBoxText = "You have unsaved changes. Do you wish to save the file?";
+                string caption = "Save";
+                MessageBoxButton button = MessageBoxButton.YesNoCancel;
+                MessageBoxImage icon = MessageBoxImage.Question;
+                MessageBoxResult result;
+
+                string savedText = string.Empty;
+
+                if (file.FilePath != "notSaved" && file.FilePath != "0")
                 {
-                    CloseTab(sender);
+                    savedText = File.ReadAllText(((TextBoxData)textBox.Tag).FilePath);
                 }
-                else
+
+                if (file.Content != savedText)
                 {
-                    PrimeEditorFile file = new PrimeEditorFile();
-                    file.FilePath = ((TextBoxData)textBox.Tag).FilePath;
-                    file.Content = textBox.Text;
+                    result = MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
 
-                    string messageBoxText = "You have unsaved changes. Do you wish to save the file?";
-                    string caption = "Save";
-                    MessageBoxButton button = MessageBoxButton.YesNoCancel;
-                    MessageBoxImage icon = MessageBoxImage.Question;
-                    MessageBoxResult result;
-
-                    string savedText = string.Empty;
-
-                    if (file.FilePath != "notSaved" && file.FilePath != "0")
+                    switch (result)
                     {
-                        savedText = File.ReadAllText(((TextBoxData)textBox.Tag).FilePath);
+                        case MessageBoxResult.Yes:
+                            SaveFile_Click(sender, e);
+                            CloseTab(sender);
+                            break;
+                        case MessageBoxResult.No:
+                            CloseTab(sender);
+                            break;
+                        default:
+                            break;
                     }
-
-                    if (file.Content != savedText)
-                    {
-                        result = MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
-
-                        switch (result)
-                        {
-                            case MessageBoxResult.Yes:
-                                SaveFile_Click(sender, e);
-                                CloseTab(sender);
-                                break;
-                            case MessageBoxResult.No:
-                                CloseTab(sender);
-                                break;
-                            default:
-                                break;
-                        }
-                    }
-                    else CloseTab(sender);
                 }
+                else CloseTab(sender);
             }
         }
 
@@ -319,13 +358,22 @@ namespace PrimeEditor
                     }
                 }
             }
-            return null; // TextBox not found
+            return null!; // TextBox not found
         }
 
         private string GetSelectedTabId()
         {
-            TabItem selectedTab = (TabItem)tabControl.SelectedItem;
-            return ((TextBoxData)selectedTab.Tag).TabId;
+            if (tabControl.SelectedItem is TabItem selectedTab)
+            {
+                if (((TextBoxData)selectedTab.Tag).TabId is string tabId)
+                {
+                    return tabId;
+                }
+            }
+            return null; // No tab selected or tab without a valid tag
+
+            //TabItem selectedTab = (TabItem)tabControl.SelectedItem;
+            //return ((TextBoxData)selectedTab.Tag).TabId;
         }
 
         private void SetSelectedTabItemHeader(string header)
