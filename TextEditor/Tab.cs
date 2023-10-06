5c5c548402a00063264412ee72be3405af059c3d
SmaxCNX <m.schmidt@connexion.at>
2023-10-06 08:46:55 +0200

    progress

--- TextEditor/Tab.cs
+++ TextEditor/Tab.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
 using System.Windows;
-using TextEditorLib.Models;
+using System.Windows.Controls;
 using System.Windows.Media;
-using System.Windows.Input;
+using TextEditorLib.Models;
 
 namespace TextEditorLib
 {
